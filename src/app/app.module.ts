1f07162fc6bcdda8e6e80f7c63588c1196aca1af
abimimi67 <alibinapi@gmail.com>
2020-07-06 19:28:38 +0200

    Premier ebauche de header, navbar, teaser, content, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontendComponent } from './components/frontend/frontend.component';
+import { BackendComponent } from './components/backend/backend.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TeaserComponent } from './components/teaser/teaser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontendComponent,
+    BackendComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent,
+    TeaserComponent
   ],
   imports: [
     BrowserModule,
